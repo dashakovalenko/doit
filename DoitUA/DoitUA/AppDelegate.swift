0b239948b6f141a57fadb0cb8a4a82580b750558
Дарья Коваленко <dk@anahoret.com>
2018-05-23 11:31:04 +0300

    add empty project

---
+++ DoitUA/DoitUA/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DoitUA
+//
+//  Created by Daria Kovalenko on 5/23/18.
+//  Copyright © 2018 Daria Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ DoitUA/DoitUA/Source/UI/ViewControllers/BaseViewController/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DoitUA
+//
+//  Created by Daria Kovalenko on 5/23/18.
+//  Copyright © 2018 Daria Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
